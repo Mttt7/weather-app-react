8dbdb373c8345eeb8f10a7130ad8ee0b4880b542
mttt <groobytt@gmail.com>
2023-04-01 18:19:37 +0200

    get rid off all drag and drop components

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState, useEffect, useRef} from 'react'
+
+
 import React from 'react';
 
 
-function WeatherCard({city, unit, cities,setCities}){
+function WeatherCard({city, unit, cities,setCities,index, indexes,setIndexes}){
 
   
   const [weather, setWeather] = useState({
@@ -28,7 +30,7 @@ function WeatherCard({city, unit, cities,setCities}){
 
 
   async function fetchImage(city){
-    return await fetch(`https://api.pexels.com/v1/search?query=${city}&page=${1}`, 
+    return await fetch(`https://api.pexels.com/v1/search?query=${city.cityName}&page=${1}`, 
     {
         method: "GET",
         headers: {
@@ -51,7 +53,7 @@ function WeatherCard({city, unit, cities,setCities}){
   }
 
   async function fetchWeather(city){
-    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=27e1317dd7a01c91b570e159116336c3`)
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.cityName}&appid=27e1317dd7a01c91b570e159116336c3`)
     .then((response)=>{
       return response.json()
     })
@@ -76,66 +78,79 @@ function WeatherCard({city, unit, cities,setCities}){
 
   function handleDoubleClick(e){
     const keyToDel=e.target.parentNode.dataset.key
-    setCities( cities.filter(c=>{return c!==keyToDel}))
+    setCities(cities.filter(c=>{return c.cityName!==keyToDel}))
+    setIndexes(indexes.filter(i=>i!==city.index))
   }
 
-
-
+  function applyLength(str){
+    try{
+      if(str.length > 14){
+      return city.cityName
+      }
+    }
+    catch(e){
+      console.log(e)
+    }
+    return str
+  }
   if(weather!==null){
     return(
-      <div className='weather-card'  data-key={city} onDoubleClick={handleDoubleClick} style={{backgroundImage: `url("${weatherImg}")`}}>
-          <div className='city-name'>{weather.city}</div>
-          <div className='current-temp'>{unit==='f' ? convertToFahrenheit(weather.temp): convertToCelsius(weather.temp)}</div>
-          <div className='min-temp'>min: {unit==='f' ? convertToFahrenheit(weather.tempMin): convertToCelsius(weather.tempMin)}</div>
-          <div className='max-temp'>max: {unit==='f' ? convertToFahrenheit(weather.tempMax): convertToCelsius(weather.tempMax)}</div>
-          <div className='wind-speed'>💨: {weather.windSpeed} m/s</div>
-          
-      </div>
+          <div className='weather-card'  data-key={city.cityName} onDoubleClick={handleDoubleClick} style={{backgroundImage: `url("${weatherImg}")`}}>
+            <div className='city-name'>{applyLength(weather.city)}</div>
+            <div className='current-temp'>{unit==='f' ? convertToFahrenheit(weather.temp): convertToCelsius(weather.temp)}</div>
+            <div className='min-temp'>min: {unit==='f' ? convertToFahrenheit(weather.tempMin): convertToCelsius(weather.tempMin)}</div>
+            <div className='max-temp'>max: {unit==='f' ? convertToFahrenheit(weather.tempMax): convertToCelsius(weather.tempMax)}</div>
+            <div className='wind-speed'>💨: {weather.windSpeed} m/s</div>
+         </div>
     )
   }
   
 }
 
-function AddWeatherCard({addCity,cities, cityExists}){
-  const [city,setCity] = useState('')
+
+
+
+
+
+function AddWeatherCard({addCity, cities, cityExists}){
+  const [name,setName] = useState('')
 
   function handleChange(e){
-    const city = e.target.value
-    setCity((prev)=>{
-      return city
+    const newName = e.target.value
+    console.log(name)
+    setName(()=>{
+      return newName
     })
   }
 
   function handleKeyPress(e){
+    console.log(name)
     if(e.key==='Enter'){
-      
+      setName('')
       if(cities.length>0){
         let unique = true
         cities.forEach(c => {
-          if(c===city){
+          if(c.cityName===name){
             unique = false
           }
         });
-        if(unique) addCity(city)
-        else cityExists(city)
+        if(unique) addCity(name)
+        else cityExists(name)
       }else{
-        addCity(city)
+        addCity(name)
       }
-      
-      
     }
   }
 
   const inputRef = useRef(null)
 
-  React.useEffect(()=>{
+  // React.useEffect(()=>{
 
-    setInterval(changePlaceholder,3500)
+  //   setInterval(changePlaceholder,3500)
 
-  },[])
+  // },[])
+
   let cityIterator = 0
-
-
   function changePlaceholder(){
     const cities = [
       'Texas',
@@ -151,18 +166,18 @@ function AddWeatherCard({addCity,cities, cityExists}){
       'New Delhi'
     ]
     
-    if(cityIterator===cities.length-1) cityIterator=0
+    if(cityIterator===cities.length-2) cityIterator=0
     inputRef.current.placeholder = cities[cityIterator]
     cityIterator++
-     
   }
 
   return(
     <div className='add-weather-card'>
-      <input type='text'  ref={(element)=>{inputRef.current = element}} placeholder="" value={city} onChange={handleChange} onKeyPress={handleKeyPress}/>
+      <input type='text'  ref={(element)=>{inputRef.current = element}} placeholder="" value={name} onChange={handleChange} onKeyPress={handleKeyPress}/>
     </div>
   )
 }
+
 
 
 function CurrentUnit({changeUnit}){
@@ -188,34 +203,49 @@ function CurrentUnit({changeUnit}){
 
 
 export default function App() {
-  const [cities, setCities] = useState([])
+  const [cities, setCities] = useState([{index:0, cityName:'Lublin'}])
   const [unit, setUnit] = useState('c')
+  const [indexes, setIndexes] = useState([])
+
+  function getIndex(){
+    let ind = 0
+    for(let i = 0;i<indexes.length;i++){
+      if(indexes[i]!==i){ 
+        setIndexes(...indexes, i)
+        setIndexes.sort()
+        return i
+      }
+    }
+    return indexes.length
+  }
 
   function changeUnit(u){
+    console.log(cities)
     if(u==='c') setUnit('c')
     else if(u==='f') setUnit('f')
   }
 
-  function handleCityAdded(city){
+  function handleCityAdded(name){
     setCities(()=>{
-      return [ city, ...cities]
+      return [ {index:getIndex, cityName:name}, ...cities]
     })
   }
-  function handleDuplicate(city){
-    console.log(`${city} already exists!`)
+  function handleDuplicate(name){
+    console.log(`${name} already exists!`)
   }
 
   return (
-   <div className='container'>
-    <AddWeatherCard  addCity={handleCityAdded} cityExists={handleDuplicate} cities={cities}/>
-    <CurrentUnit changeUnit={changeUnit}/>
-
-    
-    {cities.map(c=>{
-      return(<WeatherCard cities={cities} setCities={setCities} key={c} unit={unit} city={c}/>)
-    })}
-
-   </div>
+    <div className='container'>
+      <AddWeatherCard  addCity={handleCityAdded} cityExists={handleDuplicate} cities={cities}/>
+      <CurrentUnit changeUnit={changeUnit}/>       
+      <div className='cities-container' >
+        {cities.map((c,index)=>{
+           return(
+            <WeatherCard indexes={indexes} setIndexes={setIndexes} index={index} cities={cities} setCities={setCities} key={c.cityName} unit={unit} city={c}/>
+          )
+        })} 
+      </div>                         
+    </div>
   )
 }
 
